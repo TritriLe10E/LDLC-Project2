e8a50e33332fb7a1079e2aaff98b6935dd937d4b
TritriLe10E <tristan2003@orange.fr>
2020-11-26 20:03:57 +0100

    Vanish Timer

--- Assets/Script/VanishWall.cs
+++ Assets/Script/VanishWall.cs
@@ -4,12 +4,33 @@ using UnityEngine;
 
 public class VanishWall : MonoBehaviour
 {
-
     [SerializeField]
     GameObject wallObject;
+    float secondTimer;
+    float secondTimer2;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        wallObject.SetActive(false);
+        secondTimer = 12.0f;
+        secondTimer2 = 0.0f;
+    }
 
     private void OnTriggerStay(Collider other)
     {
-        wallObject.SetActive(false);
+        //doorObject.SetActive(false);
+        //Debug.Log("Il y a collision avec " + other.gameObject.name);
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        wallObject.SetActive(true);
+    }
+    private void Update()
+    {
+        if (secondTimer2 > secondTimer)
+        {
+            wallObject.SetActive(true);
+        }
+        secondTimer2 += Time.deltaTime;
     }
 }
