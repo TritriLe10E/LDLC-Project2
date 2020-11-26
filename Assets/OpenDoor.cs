19850a6675c82603e4858e47c1029ac69d934264
TritriLe10E <tristan2003@orange.fr>
2020-11-26 11:48:10 +0100

    Premier commit

---
+++ Assets/OpenDoor.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+    [SerializeField]
+    GameObject doorObject;
+    float timer;
+    float timer2;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        doorObject.SetActive(false);
+        timer = 2.0f;
+        timer2 = 0.0f;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        //doorObject.SetActive(false);
+        //Debug.Log("Il y a collision avec " + other.gameObject.name);
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        doorObject.SetActive(true);
+    }
+    private void Update()
+    {
+        if (timer2 > timer)
+        {
+            doorObject.SetActive(true);
+        }
+        timer2 += Time.deltaTime;
+    }
+}
+
+
---
+++ Assets/Respawn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Vector3 spawn; 
+
+    void Start()
+    {
+        spawn = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y <= -1)
+        {
+            transform.position = spawn;
+        }
+        
+        Debug.Log(transform.position);
+    }
+}
